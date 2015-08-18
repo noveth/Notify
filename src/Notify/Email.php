980f85e69e75cde098a4b5507b16546a06884d56
Norvert John <noveth2012@gmail.com>
2015-08-18 23:03:12 +0800

    Remove unnecessary functions

--- src/Notify/Email.php
+++ src/Notify/Email.php
@@ -8,65 +8,29 @@ class Email
 
   public static function send($to, $subject, $template, $vars)
   {
-      $body = self::get_template($template);
+      $template = self::get_template($template);
+      $new_template = self::set_template($template, $vars);
 
-      if (!self::send_email($to, $subject, $body, $vars)) {
+      if (!self::mail($to, $subject, $new_template)) {
           header('HTTP/1.1 500 Internal Server Error');
-          header('Content-Type: '. $this->content_type .'; charset=UTF-8');
+          header('Content-Type: '. $vars['content_type'] .'; charset=UTF-8');
           die(json_encode(array('message' => 'ERROR', 'code' => 'NS0001', 'description' => 'Email could not be sent', 'data' => $from.' '.$to.' '.$subject.' '.$message)));
           return;
       }
   }
 
-  public function send_email($to, $subject, $body, $vars)
+  public static function set_template($contents, $vars)
   {
-      if ($vars['content_type'] !== false) {
-          header('Content-Type:' . $vars['content_type']);
+      $tags = []; $data = [];
+      foreach($vars as $t => $d) {
+          array_push($tags, $t);
+          array_push($data, $d);
       }
 
-      $client_name = self::extract_name($vars['client_name']);
-      $replace  = [
-          'client_name' => $vars['client_name'],
-          'client_first_name' => $client['firstname'],
-          'client_last_name'  => $client['lastname'],
-          'client_email' => $to,
-          'service' => $vars['service'],
-          'link' => $vars['link']
-      ];
-
-      $contents = self::replace_tags($contents, $replace);
+      $contents = str_replace($tags, $data, $contents);
       $contents = str_replace("\n.", "\n..", $contents);
 
-      if (!mail($to, $subject, $body)) {
-          return false;
-      }
-  }
-
-  public function extract_name($name)
-  {
-      if ($name !== false) {
-          return false;
-      }
-
-      $newName = explode(' ',$name);
-      $name['firstname'] = ucfirst($newName[0]);
-      $name['lastname']  = ucfirst($newName[1]);
-      return $name;
-  }
-
-  public static function replace_tags($contents, $vars)
-  {
-      $tags = [
-            "{client_name}",
-            "{client_first_name}",
-            "{client_last_name}",
-            "{client_email}",
-            "{service}",
-            "{link}",
-        ];
-
-        $contents = str_replace($tags, $replace, $contents);
-        return $contents;
+      return $contents;
   }
 
   public static function get_template($template)
@@ -76,9 +40,14 @@ class Email
       }
 
       $dir = dirname(FILE) . "/templates/" . $template;
-      if (file_exists($dir)) {
-        $contents = file_get_contents($dir);
+      if (!file_exists($dir)) {
+          header('HTTP/1.1 500 Internal Server Error');
+          header('Content-Type: '. $vars['content_type'] .'; charset=UTF-8');
+          die(json_encode(array('message' => 'ERROR', 'code' => 'NS0001', 'description' => 'Cannot retrieved template')));
+          return;
       }
+
+      $contents = file_get_contents($dir);
       return $contents;
   }
 
