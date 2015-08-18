c6a87304a5402861ea0e72fb016dc18361f08389
Norvert John <noveth2012@gmail.com>
2015-08-18 23:23:47 +0800

    Add the renamed class to the external files

--- README.md
+++ README.md
@@ -9,5 +9,5 @@
 ###
 
 ```php
-$template = Notify\Email::send({to address}, {subject}, {body});
+$template = Notify\Send::email({to address}, {subject}, {body});
 ```
--- src/Notify.php
+++ src/Notify.php
@@ -1,3 +1,3 @@
 <?php
   require_once dirname(__FILE__) . '/Config.php';
-  require_once dirname(__FILE__) . '/Notify/Email.php';
+  require_once dirname(__FILE__) . '/Notify/Send.php';
