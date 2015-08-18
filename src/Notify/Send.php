2f3bdca35663adb86b6f7f3b888beca40583792c
Norvert John <noveth2012@gmail.com>
2015-08-18 22:01:37 +0800

    add function for replace_tags  and extract_name

--- src/Notify/Send.php
+++ src/Notify/Send.php
@@ -24,22 +24,50 @@ class Send
           header('Content-Type:' . $vars['content_type']);
       }
 
+      $client_name = self::extract_name($vars['client_name']);
+      $replace  = [
+          'client_name' => $vars['client_name'],
+          'client_first_name' => $client['firstname'],
+          'client_last_name'  => $client['lastname'],
+          'client_email' => $to,
+          'service' => $vars['service'],
+          'link' => $vars['link']
+      ];
+
+      $contents = self::replace_tags($contents, $replace);
+      $contents = str_replace("\n.", "\n..", $contents);
+
       if (!mail($to, $subject, $body)) {
           return false;
       }
   }
 
-  // public static function extract_sender()
-  // {
-  //   $tags = [
-  //         "{client_name}",
-  //         "{client_first_name}",
-  //         "{client_last_name}",
-  //         "{client_email}",
-  //         "{service}",
-  //         "{link}",
-  //     ];
-  // }
+  public function extract_name($name)
+  {
+      if ($name !== false) {
+          return false;
+      }
+
+      $newName = explode(' ',$name);
+      $name['firstname'] = ucfirst($newName[0]);
+      $name['lastname']  = ucfirst($newName[1]);
+      return $name;
+  }
+
+  public static function replace_tags($contents, $vars)
+  {
+      $tags = [
+            "{client_name}",
+            "{client_first_name}",
+            "{client_last_name}",
+            "{client_email}",
+            "{service}",
+            "{link}",
+        ];
+
+        $contents = str_replace($tags, $replace, $contents);
+        return $contents;
+  }
 
   public static function get_template($template)
   {
@@ -50,7 +78,6 @@ class Send
       $dir = dirname(FILE) . "/templates/" . $template;
       if (file_exists($dir)) {
         $contents = file_get_contents($dir);
-        $contents = str_replace("\n.", "\n..", $contents);
       }
       return $contents;
   }
