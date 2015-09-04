140904d0624da346d2b88d4de8de66eaec9c988a
Luke Bickley <luke.bickley@fatjoe.co.uk>
2015-09-04 16:29:30 +0100

    Added additional config items so we can set a global from address and tell if its a HTML email or not

--- src/Config.php
+++ src/Config.php
@@ -4,7 +4,11 @@
   class Config
   {
     public static $DIRNAME;
+    public static $FROM;
+    public static $ISHTML;
   }
 
   Config::$DIRNAME = dirname(__FILE__);
+  Config::$FROM = 'luke.bickley@fatjoe.co';
+  Config::$ISHTML = 1;
 ?>
