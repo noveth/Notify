6c80cbc5bcae4eef809e93c387ea9d04a16b6fbc
Luke Bickley <luke.bickley@fatjoe.co.uk>
2015-09-04 15:35:03 +0100

    Removed a define variable (needs to be done in an alternate way

--- src/Notify.php
+++ src/Notify.php
@@ -1,5 +1,3 @@
 <?php
-  define('ROOTPATH', $_SERVER['DOCUMENT_ROOT'] . $_SERVER['REQUEST_URI']);
-
   require_once dirname(__FILE__) . '/Config.php';
   require_once dirname(__FILE__) . '/Notify/Email.php';
