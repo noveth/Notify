3e31a2410c93865add23e324793c65acd8bf324d
Luke Bickley <luke.bickley@fatjoe.co.uk>
2015-09-04 16:13:17 +0100

    Fixed Exceptions getting caught in the namespace.
    Re-tabbed the file.

--- src/Notify/Email.php
+++ src/Notify/Email.php
@@ -7,48 +7,50 @@ class Email
 {
   public static function send($to, $subject, $template, $vars = null)
   {
-      $template = self::get_template($template, $vars);
+    $template = self::get_template($template, $vars);
 
-      if ($template === false) {
-        return false;
-      }
+    if ($template === false) {
+      return false;
+    }
 
-      if (!mail($to, $subject, $template)) {
-          throw new Exception("Email could not be sent", true);
-          return false;
-      }
-      return true;
+    if (!mail($to, $subject, $template)) {
+      throw new \Exception("Email could not be sent");
+      return false;
+    }
+    return true;
   }
 
   private static function set_template($contents, $vars)
   {
-      if ($vars) {
-          $tags = []; $data = [];
-          foreach($vars as $t => $d) {
-              $tags[] = $t;
-              $data[] = $d;
-          }
+    if ($vars) {
+      $tags = []; $data = [];
+      foreach($vars as $t => $d) {
+        $tags[] = $t;
+        $data[] = $d;
       }
+    }
 
-      $contents = str_replace($tags, $data, $contents);
-      $contents = str_replace("\n.", "\n..", $contents);
+    $contents = str_replace($tags, $data, $contents);
+    $contents = str_replace("\n.", "\n..", $contents);
 
-      return $contents;
+    return $contents;
   }
 
   private static function get_template($template, $vars)
   {
-      if (!$template) {
-        return false;
-      }
+    if (!$template) {
+      return false;
+    }
 
-      $dir = Config::$DIRNAME . 'templates/' . $template;
-      if (!file_exists($dir)) {
-        throw new Exception("Specified template could not be found");
-        return false;
-      }
+    $dir = Config::$DIRNAME . '/templates/' . $template;
 
-      $contents = file_get_contents($dir);
-      return self::set_template($contents, $vars);
+    if (!file_exists($dir)) {
+      throw new \Exception("Specified template could not be found");
+      return false;
+    }
+
+    $contents = file_get_contents($dir);
+
+    return self::set_template($contents, $vars);
   }
 }
