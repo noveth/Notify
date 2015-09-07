c6e1c8a210cbcee991db946fcace7178006255bb
Luke Bickley <luke.bickley@fatjoe.co.uk>
2015-09-07 15:43:45 +0100

    Fixed Loader

--- src/Notify/Loader.php
+++ src/Notify/Loader.php
@@ -2,22 +2,10 @@
 namespace Notify;
 use Notify\Config;
 
-Class Loader
-{
-  public function __contruct()
-  {
-    $this->init();
-  }
-
-  public function init()
-  {
-    if (Config\$EMAIL === true) {
-      include(dirname(__FILE__) . 'Email.php');
-    }
-
-    if (Config\$SLACK === true) {
-      include(dirname(__FILE__) . 'Slack.php');
-    }
-  }
+if (Config::$EMAIL === true) {
+  include(dirname(__FILE__) . '/Email.php');
+}
+if (Config::$SLACK === true) {
+  include(dirname(__FILE__) . '/Slack.php');
 }
 ?>
