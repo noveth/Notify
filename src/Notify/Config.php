a56643ac55fb78685e8a40ef7f1c047d4326a003
Luke Bickley <luke.bickley@fatjoe.co.uk>
2015-09-07 14:46:32 +0100

    Updated base config

--- src/Notify/Config.php
+++ src/Notify/Config.php
@@ -3,9 +3,21 @@ namespace Notify;
 
 class Config
 {
+  // Modules
+  public static $EMAIL;
+  public static $SLACK;
+
+  // Email
   public static $DIRNAME;
   public static $FROM;
+
+  // Slack
+  public static $SLACKWEBHOOK;
+  public static $SLACKUSERNAME;
 }
+
+Config::$EMAIL = true;
+Config::$SLACK = true;
 
 Config::$DIRNAME = dirname(__FILE__) . '/..';
 Config::$FROM = 'example@example.co.uk';
